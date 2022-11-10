d5a6585999c231570651c58363d78cf0af3b6cd2
Chris Cromer <chris@cromer.cl>
2022-11-10 00:10:05 -0300

    add some signals to the event bus

---
+++ scripts/Event.cs
@@ -0,0 +1,15 @@
+using Godot;
+
+public sealed class Event : Node
+{
+	[Signal]
+	delegate void EnergyCollected(int energy);
+	[Signal]
+	delegate void BoltCollected(int bolt);
+	[Signal]
+	delegate void InvincibilityCollected();
+	[Signal]
+	delegate void FireCollected();
+	[Signal]
+	delegate void PlayerTouched();
+}
