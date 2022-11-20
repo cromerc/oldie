8625181b1ab2dd08aca3d72dc7398123b8fdb977
Chris Cromer <chris@cromer.cl>
2022-11-19 23:21:06 -0300

    add bolt and checkpoint logic to the main game logic

--- scenes/game/Game.cs
+++ scenes/game/Game.cs
@@ -7,25 +7,37 @@ public sealed class Game : Node
     [Export]
     private int _bolts { get; set; } = 0;
 
-    public enum PhysicsLayer : ushort {
+    public enum PhysicsLayer : ushort
+    {
         Player = 1,
         Platform = 2,
         Enemy = 3,
-        Collectable = 4
+        Collectable = 4,
+        Checkpoint = 5
     }
 
     public override void _Ready()
     {
         var eventBus = GetNode<Event>("/root/Event");
         eventBus.Connect("EnergyCollected", this, "OnEnergyCollected");
+        eventBus.Connect("BoltCollected", this, "OnBoltCollected");
     }
 
-    private void OnEnergyCollected(int energy)
+    public void OnEnergyCollected(int energy)
     {
         _energy += energy;
         if (OS.IsDebugBuild())
         {
             GD.Print("Energy: " + _energy);
+        }
+    }
+
+    public void OnBoltCollected(int bolts)
+    {
+        _bolts += bolts;
+        if (OS.IsDebugBuild())
+        {
+            GD.Print("Bolts: " + _bolts);
         }
     }
 }
